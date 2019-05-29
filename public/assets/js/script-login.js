90581d6d8d41213e9028fd52f9cc2d4cce6b7371
Gabriele Gullì <gabrielegulli97@gmail.com>
2019-05-29 14:48:51 +0200

    Log in alert try

--- public/assets/js/script-login.js
+++ public/assets/js/script-login.js
@@ -32,10 +32,10 @@ $("#log-in").click(function(){
     }
 
     if (!done){
-        //alert("Email or Password not correct, please try again.");
-        $("#ModalTitle").html("Attention");
-        $("#modalContent").html("Email or Password not correct, please try again.");
-        $( "#modalButton" ).trigger( "click" );
+        alert("Email or Password not correct, please try again.");
+        //$("#ModalTitle").html("Attention");
+        //$("#modalContent").html("Email or Password not correct, please try again.");
+        //$( "#modalButton" ).trigger( "click" );
     }else {
         //Redirect home
         window.location.href = "../index.html";
